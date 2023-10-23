98037b35191a54e84111c03598437831935ee987
Welkus <vdelkus@gmail.com>
2023-10-23 16:28:49 +0300

    Task Done

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,6 +2,11 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
+        int sum = 1;
+        for (int i = 1; i <= power;i++){
+            sum *= numberToPrint;
+        }
+        System.out.println(sum);
 
     }
 
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -3,9 +3,25 @@ package school.mjc.stage0.loops.finalTask;
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
 
+
+        for (int i = 1; i <= cathetusLength; i++) {
+
+            for (int space = 1; space <= cathetusLength - i; space++) {
+                System.out.print(" ");
+            }
+            for (int num = i; num >= 1; num--) {
+                System.out.print(num);
+            }
+            for (int k = 2; k <= i; k++) {
+                System.out.print(k);
+            }
+            System.out.println();
+
+        }
     }
 
-    public static void main(String[] args) {
-        new Pyramid().printPyramid(7);
+        public static void main (String[]args){
+            new Pyramid().printPyramid(7);
+        }
     }
-}
+
